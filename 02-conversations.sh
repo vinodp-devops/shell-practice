65930f8e03827581c7333330cdafc801606bd028
vinodp-devops <pasupuletivinod8@gmail.com>
2026-03-29 20:18:22 +0530

    Shell echo conversations

---
+++ 02-conversations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Rama:: Hello Vinod, how are you?"
+echo "Vinod:: Hi Rama, I am fine thanks for asking. How are you doing?"
+echo "Rama:: Doing fine. What are you learning Vinod?"
+echo "Vinod:: I am learning Shell. What about you?"
