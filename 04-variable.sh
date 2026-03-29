f78d15b9de4cc7eea5e36b637b1bec34e6f70d26
vinodp-devops <pasupuletivinod8@gmail.com>
2026-03-29 21:02:10 +0530

    Shell: 04-Variable args echo conversations

--- 04-variable.sh
+++ 04-variable.sh
@@ -7,3 +7,5 @@ echo "$PERSON1:: Hello $PERSON2, how are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
 echo "$PERSON1:: Doing fine. What are you learning $PERSON2?"
 echo "$PERSON2:: I am learning Shell. What about you?"
+
+#sh 04-variables.sh Rama Vinod
