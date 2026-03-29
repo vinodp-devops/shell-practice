d98dd48b3d87456dcb35adcc53b47845ac939c0c
vinodp-devops <pasupuletivinod8@gmail.com>
2026-03-29 20:50:02 +0530

    Shell: Variable echo conversations

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Rama #no space between value and =
+PERSON2=Vinod
+
+echo "$PERSON1:: Hello PERSON2, how are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
+echo "$PERSON1:: Doing fine. What are you learning PERSON2?"
+echo "PERSON2:: I am learning Shell. What about you?"
