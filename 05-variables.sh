28de83e558e8602812aca10e22bc5edba284dd17
vinodp-devops <pasupuletivinod8@gmail.com>
2026-03-29 21:22:06 +0530

    Shell: 05-Variable username & Password -s echo conversations

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
- echo "Please Enter your username:"
- read USER_NAME
+echo "Please Enter your username:"
+read USER_NAME
+echo "User name is $USER_NAME"
 
- echo "User name is $USER_NAME"
-
- echo "Please Enter your Password:"
- read -s PASSWORD
-
+echo "Please Enter your Password:"
+read -s PASSWORD
+echo "Password is $PASSWORD"
