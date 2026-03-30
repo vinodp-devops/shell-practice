573a26dcf3fe67353cf34a66557ee2630cfcbc82
vinodp-devops <pasupuletivinod8@gmail.com>
2026-03-30 07:38:18 +0530

    Shell: 09-conditions #-gt, -lt, -eq, -ne

---
+++ 09-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUMBER=$1
+
+#-gt -> greater than
+#-lt -> less than
+#-eq -> equals
+#-ne -> not equal
+if [ $NUMBER -gt 20 ]; then
+    echo "Given number: $NUMBER is greater than 20"
+elif[ $NUMBER -eq 20 ]; then
+    echo "Given number: $NUMBER is equals to 20"
+else
+    echo "Given number: $NUMBER is less than 20"
