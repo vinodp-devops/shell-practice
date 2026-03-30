a4bf5e85623825f59c4556f542d0e0c474ba23aa
vinodp-devops <pasupuletivinod8@gmail.com>
2026-03-30 08:30:16 +0530

    Shell: 07-Variables Special Variables: , , 0, /usr/bin/bash, /d/sivakumar_aws/repos/shell-practice, , /c/Users/VINOD, 488, sleep 100 &, , 0 run shell file args

--- 07-variables.sh
+++ 07-variables.sh
@@ -11,5 +11,11 @@ echo "Home directory of current user: $HOME"
 echo "PID of this script: $$"
 sleep 100 &
 echo "PID of recently executed background process: $!"
+echo "Exit status of previous command: $?"
+#$?
+#============
+#0-127
+#0 -> Success
+# Anything else -> failure
 
 # sh 07-variables Rama Vinod
