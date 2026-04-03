7891263afb1e3262912a945f12080701a7d5b5ef
vinodp-devops <pasupuletivinod8@gmail.com>
2026-04-04 02:28:36 +0530

    Shell: 20-trap, best & safe way example & removed VALIDATE function

--- 20-trap.sh
+++ 20-trap.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-set -e #ERR
-trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
-
+set -e
+trap 'echo "There is an error in line $LINENO, Command: $BASH_COMMAND"' ERR
 
 R="\e[31m"
 G="\e[32m"
@@ -10,27 +9,25 @@ Y="\e[33m"
 P="\e[35m"
 N="\e[0m"
 
-# Gets the Userid root -> 0
 USERID=$(id -u)
+
 LOGS_FOLDER="/var/log/shell-script"
-LOGS_FILE="$LOGS_FOLDER/$0.log"
+SCRIPT_NAME=$(basename "$0")
+LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
+mkdir -p "$LOGS_FOLDER"
 
-if [ $USERID != 0 ]; then
-    echo -e "$R Please run this script with root user access: sudo su - $N" | tee -a $LOGS_FILE
+if [ "$USERID" -ne 0 ]; then
+    echo -e "${R}Please run this script with root access${N}" | tee -a "$LOGS_FILE"
     exit 1
 fi
 
-mkdir -p $LOGS_FOLDER
-
-# sudo sh 15-loops.sh nginx mysql nodejs
-for package in $@
+for package in "$@"
 do
-    dnf list installed $package &>> $LOGS_FILE
-    if [ $? != 0 ]; then
-        echo -e "$P $package not installed, installation started... $N"
-        dnf install $package -y &>> $LOGS_FILE
+    if ! dnf list installed "$package" &>> "$LOGS_FILE"; then
+        echo -e "${P}$package not installed, installation started...${N}"
+        dnf install "$package" -y &>> "$LOGS_FILE"
     else
-        echo -e "$package already installed, $Y SKIPPING $N"
+        echo -e "$package already installed, ${Y}SKIPPING${N}"
     fi
 done
