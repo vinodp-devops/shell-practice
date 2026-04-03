f70e80ddd301db05431576b5847d9ac52dc992ae
vinodp-devops <pasupuletivinod8@gmail.com>
2026-04-03 21:20:03 +0530

    Shell: 12-logs, CMD 12&>> output.txt, tee -a log_file mkdir in -p /var/log/shell-scrpt//usr/bin/bash.log and store logs

--- 12-logs.sh
+++ 12-logs.sh
@@ -3,7 +3,7 @@
 # Gets the Userid root -> 0
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
-LOGS_FILENAME="$LOGS_FOLDER/$0.log"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
 
 if [ $USERID != 0 ]; then
     echo "Please run this script with root user access: sudo su -"
@@ -12,36 +12,36 @@ fi
 
 mkdir -p $LOGS_FOLDER
 if [ $? != 0 ]; then
-    echo "Unable to create Directory $LOGS_FOLDER"
+    echo "Unable to create Directory $LOGS_FOLDER" | tee -a $LOG_FILE
     exit 1
 else
-    echo "Directory $LOGS_FOLDER CREATED"
+    echo "Directory $LOGS_FOLDER CREATED/Detected" | tee -a $LOG_FILE
 fi
 
 VALIDATE() {
     if [ $1 != 0 ]; then
-        echo "$2 ... FAILURE"
+        echo "$2 ... FAILURE" | tee -a $LOG_FILE
         exit 1
     else
-        echo "$2 ... SUCCESS"
+        echo "$2 ... SUCCESS" | tee -a $LOG_FILE
     fi
 }
 
 VALIDATE2() {
-    echo "$1 UNINSTALLATION ...SUCCESS "
+    echo "$1 UNINSTALLATION ...SUCCESS" | tee -a $LOG_FILE 
 }
 
 
-dnf install nginx -y &>> $LOGS_FILENAME
+dnf install nginx -y &>> $LOGS_FILE
 VALIDATE $? "Installing Nginx"
 
-dnf install mysql -y &>> $LOGS_FILENAME
+dnf install mysql -y &>> $LOGS_FILE
 VALIDATE $? "Installing MySQL"
 
-dnf install nodejs -y &>> $LOGS_FILENAME
+dnf install nodejs -y &>> $LOGS_FILE
 VALIDATE $? "Installing NodeJS"
 
-dnf remove nginx -y &>> $LOGS_FILENAME
-dnf remove mysql -y &>> $LOGS_FILENAME
-dnf remove nodejs -y &>> $LOGS_FILENAME
+dnf remove nginx -y &>> $LOGS_FILE
+dnf remove mysql -y &>> $LOGS_FILE
+dnf remove nodejs -y &>> $LOGS_FILE
 VALIDATE2 "Done"
