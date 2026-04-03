b7363a27b906347224d0342a24a7ec4e81356e9d
vinodp-devops <pasupuletivinod8@gmail.com>
2026-04-04 02:15:34 +0530

    Shell: 20-trap, example & removed VALIDATE function

---
+++ 20-trap.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e #ERR
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR &>> $LOGS_FILE
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+P="\e[35m"
+N="\e[0m"
+
+# Gets the Userid root -> 0
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+if [ $USERID != 0 ]; then
+    echo -e "$R Please run this script with root user access: sudo su - $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+# sudo sh 15-loops.sh nginx mysql nodejs
+for package in $@
+do
+    dnf list installed $package &>> $LOGS_FILE
+    if [ $? != 0 ]; then
+        echo -e "$P $package not installed, installation started... $N"
+        dnf install $package -y &>> $LOGS_FILE
+    else
+        echo -e "$package already installed, $Y SKIPPING $N"
+    fi
+done
